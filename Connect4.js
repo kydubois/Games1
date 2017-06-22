21bf205cf740010da0705ebc8bdda7475cef2bf8
kydubois <kydubois28@gmail.com>
2017-06-22 13:07:00 -0500

    Connect4.js
    uploade JavaScript file for a connect four game

---
+++ Connect4.js
@@ -0,0 +1,11 @@
+// ASSIGN PAYER COLORS
+
+// GRAB ALL THE CIRCLES
+var circles = $('td').document
+
+// CHECK THE CIRCLE MARKER
+
+//CHANGE THE COLOR
+$('td').on('click', function() {
+  $(this).toggleClass('turnBlue')
+})
