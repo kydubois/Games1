6e98b5fda3772592b08bb853531c7d9076e00aa8
kydubois <kydubois28@gmail.com>
2017-06-21 20:52:02 -0500

    Updated TicTacToe.js
    added cellFour through cellNine

--- TicTacToe.js
+++ TicTacToe.js
@@ -51,6 +51,78 @@ var cellThree = document.querySelector('#three')
    }
  })
 
+var cellFour = document.querySelector('#four')
+
+ cellFour.addEventListener('click',function() {
+   if (cellFour.textContent === '') {
+     cellFour.textContent = 'X';
+   }else if (cellThree.textContent === 'X') {
+     cellFour.textContent = 'O';
+   }else {
+     cellFour.textContent = '';
+   }
+ })
+
+var cellFive = document.querySelector('#five')
+
+ cellFive.addEventListener('click',function() {
+   if (cellFive.textContent === '') {
+     cellFive.textContent = 'X';
+   }else if (cellFive.textContent === 'X') {
+     cellFive.textContent = 'O';
+   }else {
+     cellFive.textContent = '';
+   }
+ })
+
+var cellSix = document.querySelector('#six')
+
+ cellSix.addEventListener('click',function() {
+   if (cellSix.textContent === '') {
+     cellSix.textContent = 'X';
+   }else if (cellSix.textContent === 'X') {
+     cellSix.textContent = 'O';
+   }else {
+     cellSix.textContent = '';
+   }
+ })
+
+var cellSeven = document.querySelector('#seven')
+
+ cellSeven.addEventListener('click',function() {
+   if (cellSeven.textContent === '') {
+     cellSeven.textContent = 'X';
+   }else if (cellSeven.textContent === 'X') {
+     cellSeven.textContent = 'O';
+   }else {
+     cellSeven.textContent = '';
+   }
+ })
+
+var cellEight = document.querySelector('#eight')
+
+ cellEight.addEventListener('click',function() {
+   if (cellEight.textContent === '') {
+     cellEight.textContent = 'X';
+   }else if (cellEight.textContent === 'X') {
+     cellEight.textContent = 'O';
+   }else {
+     cellEight.textContent = '';
+   }
+ })
+
+var cellNine = document.querySelector('#nine')
+
+ cellNine.addEventListener('click',function() {
+   if (cellNine.textContent === '') {
+     cellNine.textContent = 'X';
+   }else if (cellNine.textContent === 'X') {
+     cellNine.textContent = 'O';
+   }else {
+     cellNine.textContent = '';
+   }
+ })
+
 // FOR LOOP TO ADD EVENT LISTENERS TO ALL THE SQUARES
 for (var i = 0; i < squares.length; i++) {
   squares[i].addEventListener('click', changeMarker)
