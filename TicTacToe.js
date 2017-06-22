7099b15ed7439d221c49f6de3a2bad1c3294c948
kydubois <kydubois28@gmail.com>
2017-06-21 20:30:42 -0500

    Update TicTacToe.js
    added created cellTwo and cellThree

--- TicTacToe.js
+++ TicTacToe.js
@@ -27,9 +27,31 @@ var cellOne = document.querySelector('#one')
    }
  })
 
+var cellTwo = document.querySelector('#two')
 
+ cellTwo.addEventListener('click',function() {
+   if (cellTwo.textContent === '') {
+     cellTwo.textContent = 'X';
+   }else if (cellTwo.textContent === 'X') {
+     cellTwo.textContent = 'O';
+   }else {
+     cellTwo.textContent = '';
+   }
+ })
 
-// FOR LOOP TO ADD ECENT LISTENERS TO ALL THE SQUARES
-for (var i = 0; i < squares.lenght; i++) {
+var cellThree = document.querySelector('#three')
+
+ cellThree.addEventListener('click',function() {
+   if (cellThree.textContent === '') {
+     cellThree.textContent = 'X';
+   }else if (cellThree.textContent === 'X') {
+     cellThree.textContent = 'O';
+   }else {
+     cellThree.textContent = '';
+   }
+ })
+
+// FOR LOOP TO ADD EVENT LISTENERS TO ALL THE SQUARES
+for (var i = 0; i < squares.length; i++) {
   squares[i].addEventListener('click', changeMarker)
 }
