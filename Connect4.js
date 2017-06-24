cfbe95aa0e01d10e2170e593643a6855bb4a5cdb
kydubois <kydubois28@gmail.com>
2017-06-24 12:36:27 -0500

    Update Connect4.js

--- Connect4.js
+++ Connect4.js
@@ -35,13 +35,3 @@ $('.board button').on('click', function() {
     currentColor = playerTneColor;
   }
 })
-
-// GRAB ALL THE CIRCLES
-var circles = $('td').document
-
-// CHECK THE CIRCLE MARKER
-
-//CHANGE THE COLOR
-$('td').on('click', function() {
-  $(this).toggleClass('turnBlue')
-})
